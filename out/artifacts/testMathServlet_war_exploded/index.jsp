5ef6364a1b8a34bdf883901fa5d62d13a79ecb1f
ovelychko <Alex.Velichko@gmail.com>
2021-01-02 10:09:07 +0200

    initial version

---
+++ out/artifacts/testMathServlet_war_exploded/index.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 38095
+  Date: 12/29/2020
+  Time: 05:03
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>testMathServlet</title>
+  </head>
+  <body>
+  testMathServlet works, test it:<br>
+  <a href="http://localhost:8080/testMathServlet_war_exploded/sum?a=5&b=4">sum</a> <br>
+  <a href="http://localhost:8080/testMathServlet_war_exploded/mult?a=5&b=4">mult</a> <br>
+  </body>
+</html>
---
+++ src/main/java/org/ovelychko/addition.java
@@ -0,0 +1,76 @@
+package org.ovelychko;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class addition extends HttpServlet {
+
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html;charset=utf-8");
+
+        PrintWriter pw = resp.getWriter();
+
+        int a, b, c;
+
+        String param_a = req.getParameter("a");
+        if (param_a == null || param_a.isEmpty()) {
+            pw.println("error: A is empty:" + param_a);
+            return;
+        }
+
+        String param_b = req.getParameter("b");
+        if (param_b == null || param_b.isEmpty()) {
+            pw.println("error: B is empty:" + param_b);
+            return;
+        }
+
+        try {
+            a = Integer.parseInt(param_a);
+            b = Integer.parseInt(param_b);
+        }
+        catch (NumberFormatException e) {
+            pw.println("error: parameter parse :" + param_a + " / " + param_b);
+            return;
+        }
+
+        c = a + b;
+        pw.println(c);
+    }
+
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html;charset=utf-8");
+
+        PrintWriter pw = resp.getWriter();
+
+        int a, b, c;
+
+        String param_a = req.getParameter("a");
+        if (param_a == null || param_a.isEmpty()) {
+            pw.println("error: A is empty:" + param_a);
+            return;
+        }
+
+        String param_b = req.getParameter("b");
+        if (param_b == null || param_b.isEmpty()) {
+            pw.println("error: A is empty:" + param_b);
+            return;
+        }
+
+        try {
+            a = Integer.parseInt(param_a);
+            b = Integer.parseInt(param_b);
+        }
+        catch (NumberFormatException e) {
+            pw.println("error: parameter parse :" + param_a + " / " + param_b);
+            return;
+        }
+
+        c = a + b;
+        pw.println(c);
+    }
+}
